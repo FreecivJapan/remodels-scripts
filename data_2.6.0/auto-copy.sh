d50b5ccedcf6e209fbd15832b9161c0b3d755998
i-makinori <maau3p@gmail.com>
2019-11-29 22:17:52 +0900

    Add auto-copy.sh to copy decided files to freeciv's data directory

---
+++ data_2.6.0/auto-copy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/sh
+
+
+# files or directories to copy
+TO_COPY=(akasaba akasaba.serv newteiki)
+
+
+# Freeciv's data directory of Linux
+FREECIVS_DATA_DIRECTORY=/usr/local/share/freeciv/
+
+
+# freeciv-scripts/scripts/files
+BASE_DIR=$(dirname "$0")
+
+
+
+# make symlinks
+
+for file in ${TO_COPY[@]}
+do
+  echo "copy $file to $FREECIVS_DATA_DIRECTORY"
+  cp -rf $file $FREECIVS_DATA_DIRECTORY
+done
+
